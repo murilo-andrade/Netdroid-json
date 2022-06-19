4696de22bbcb383a3ef1bda8e783f9f7a3a7791a
murilo-andrade <murilo.andrade@gmail.com>
2022-06-19 02:33:20 -0300

    All vars

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
         header("Content-Type: application/json");
-        $arr = array('Client' => array(
+/*         $arr = array('Client' => array(
                 0 => array('host' => $_SERVER['REMOTE_ADDR']),
                 1 => array('agent' => $_SERVER['HTTP_USER_AGENT'])
         ));
+ */
 
-
-echo json_encode($arr);
+        $arr = array('Client' => $_SERVER);
+        echo json_encode($arr);
 ?>
